fc77f240c7d28ac44e31fb92d56f3b52e7d54b11
jiny <zumod3naranja@gmail.com>
2023-12-16 23:09:43 +0900

    first commit

---
+++ src/test/java/com/dish/perfect/PerfectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dish.perfect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
