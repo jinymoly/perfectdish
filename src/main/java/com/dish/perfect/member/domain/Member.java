bca91e8ce18b29676bf8e283576c88cfcae3940d
jiny <zumod3naranja@gmail.com>
2023-12-31 23:53:34 +0900

    Feat : add member

---
+++ src/main/java/com/dish/perfect/member/domain/Member.java
@@ -0,0 +1,29 @@
+package com.dish.perfect.member.domain;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.dish.perfect.image.domain.UploadFile;
+
+public class Member {
+    
+    private Long id;
+
+    private String nickname;
+    private String password;
+
+    private LocalDateTime createAt;
+
+    private List<UploadFile> imageFiles;
+
+    public Member(Long id, String nickname, String password, LocalDateTime createAt, List<UploadFile> imageFiles) {
+        this.id = id;
+        this.nickname = nickname;
+        this.password = password;
+        this.createAt = LocalDateTime.now();
+        this.imageFiles = imageFiles;
+    }
+
+    
+    
+}
