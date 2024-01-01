2fc4bee8ad47ca01e75eb7b061ae8472a192dbcc
jiny <zumod3naranja@gmail.com>
2024-01-01 23:46:27 +0900

    Feat : add course

---
+++ src/main/java/com/dish/perfect/menu/domain/Course.java
@@ -0,0 +1,14 @@
+package com.dish.perfect.menu.domain;
+
+import lombok.Data;
+
+@Data
+public class Course {
+
+    private CourseType type;
+    private String title;
+    private String description;
+    
+
+
+}
