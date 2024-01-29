12edf551815c8c4012490b6e9e78a50269f00a76
jiny <zumod3naranja@gmail.com>
2024-01-29 23:58:59 +0900

    Feat : globalException init

--- src/main/java/com/dish/perfect/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/dish/perfect/global/error/GlobalExceptionHandler.java
@@ -4,16 +4,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.dish.perfect.global.error.exception.MemberException;
-
 import lombok.extern.slf4j.Slf4j;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
     
-    @ExceptionHandler(MemberException.class)
-    protected ResponseEntity<ErrorResponse> handleMemberException(final MemberException memberException){
+    @ExceptionHandler(GlobalException.class)
+    protected ResponseEntity<ErrorResponse> handleMemberException(final GlobalException memberException){
         log.warn(memberException.getMessage(), memberException);
         
         return ErrorResponse.toResponseEntity(memberException.getErrorCode());
