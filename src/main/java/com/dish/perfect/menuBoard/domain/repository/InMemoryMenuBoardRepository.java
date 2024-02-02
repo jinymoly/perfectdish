221337c84cd5fce46c41c2a88db9ebe10c53d090
jiny <zumod3naranja@gmail.com>
2024-02-02 17:38:09 +0900

    Feat : menuBoard repo 로직

---
+++ src/main/java/com/dish/perfect/menuBoard/domain/repository/InMemoryMenuBoardRepository.java
@@ -0,0 +1,61 @@
+package com.dish.perfect.menuBoard.domain.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import com.dish.perfect.menu.domain.Menu;
+import com.dish.perfect.menuBoard.dto.request.MenuBoardRequest;
+
+@Repository
+public class InMemoryMenuBoardRepository implements MenuBoardRepository{
+
+    List<Menu> commonMenus;
+    Optional<List<Menu>> discountMenus;
+    
+    List<Menu> allMenuList;
+
+    @Override
+    public void addCommonMenu(MenuBoardRequest menuBoardRequest) {
+        Menu menu = menuBoardRequest.getMenu();
+        if(commonMenus.isEmpty()){
+            commonMenus = new ArrayList<>();
+        }
+        commonMenus.add(menu);
+    }
+
+    @Override
+    public void addDiscountMenu(MenuBoardRequest menuBoardRequest) {
+        Menu menu = menuBoardRequest.getMenu();
+        if(discountMenus.isEmpty()){
+            List<Menu> newList = new ArrayList<>();
+            newList.add(menu);
+            discountMenus = Optional.of(newList);
+        } else {
+            discountMenus.ifPresent(menus -> menus.add(menu));
+        }
+    }
+
+    @Override
+    public List<Menu> findCommonMenus() {
+        return commonMenus;
+    }
+
+    @Override
+    public Optional<List<Menu>> findDiscountMenus() {
+        return discountMenus;
+    }
+
+    @Override
+    public List<Menu> findAllMenus() {
+        if(allMenuList.isEmpty()){
+            allMenuList = new ArrayList<>();
+        } else{
+            allMenuList.addAll(commonMenus);
+            discountMenus.ifPresent(allMenuList::addAll);
+        }
+        return allMenuList;
+    }
+}
