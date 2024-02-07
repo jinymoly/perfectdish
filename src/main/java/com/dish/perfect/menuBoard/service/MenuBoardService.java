6bb828d45d6e3ca6f05a0da71a89cbd13334c422
jiny <zumod3naranja@gmail.com>
2024-02-07 21:49:54 +0900

    Feat : MenuBoardService 예외처리 및 로그 정리

--- src/main/java/com/dish/perfect/menuBoard/service/MenuBoardService.java
+++ src/main/java/com/dish/perfect/menuBoard/service/MenuBoardService.java
@@ -7,22 +7,29 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.dish.perfect.global.error.GlobalException;
+import com.dish.perfect.global.error.exception.ErrorCode;
 import com.dish.perfect.menu.domain.Menu;
 import com.dish.perfect.menuBoard.domain.repository.MenuBoardRepository;
 import com.dish.perfect.menuBoard.dto.request.MenuBoardRequest;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class MenuBoardService {
     
     private final MenuBoardRepository menuBoardRepository;
 
     public void saveCommonMenus(MenuBoardRequest mBoardRequest){
+        if(mBoardRequest.getCommonMenus() == null){
+            log.error("🚨 {}", "commMenu 생성 불가.");
+            throw new GlobalException(ErrorCode.FAIL_CREATE_MENU_BOARD);
+        }
         menuBoardRepository.addCommonMenu(mBoardRequest);
-        
-    }
+    }        
 
     public void saveDiscountMenus(MenuBoardRequest mBoardRequest){
         menuBoardRepository.addDiscountMenu(mBoardRequest);
@@ -30,16 +37,25 @@ public class MenuBoardService {
 
 
     public List<Menu> commonMenus(){
+        if(commonMenus() == null){
+            log.error("🚨 {}", "commonMenu를 찾을 수 없습니다.");
+            throw new  GlobalException(ErrorCode.NOT_FOUND_MENU_BOARD_COMMONS);
+        }
         return menuBoardRepository.findCommonMenus();
     }
 
     public List<Menu> discountMenus(){
         Optional<List<Menu>> optionalMenus = menuBoardRepository.findDiscountMenus();
-        return optionalMenus.orElseThrow(() -> new NoSuchElementException("오늘의 할인 메뉴가 존재하지 않습니다."));
+        return optionalMenus.orElseThrow(() -> new GlobalException(ErrorCode.NOT_FOUND_MENU_BOARD_DISCOUNTS));
     }
 
     public List<Menu> getAllMenus(){
-        return menuBoardRepository.findAllMenus();
+        try {
+            return menuBoardRepository.findAllMenus();
+        } catch (NoSuchElementException e) {
+            log.error("🚨 {}", "menuBoard가 존재하지 않습니다.");
+            throw new GlobalException(ErrorCode.NOT_FOUND_MENU_BOARD);
+        }
     }
 
 }
