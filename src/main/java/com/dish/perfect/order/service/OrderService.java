86137e5ca2f9c6cfcf0dda5ad29b4165793a21e9
jiny <zumod3naranja@gmail.com>
2024-02-22 01:44:21 +0900

    Feat : Order 최종 주문 플로우 - 타입 변환 생성

--- src/main/java/com/dish/perfect/order/service/OrderService.java
+++ src/main/java/com/dish/perfect/order/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.dish.perfect.order.service;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -7,9 +9,12 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Service;
 
+import com.dish.perfect.order.domain.Order;
+import com.dish.perfect.order.domain.OrderStatus;
 import com.dish.perfect.order.domain.repository.OrderRepository;
 import com.dish.perfect.order.dto.request.OrderRequest;
 import com.dish.perfect.orderItem.domain.OrderItem;
+import com.dish.perfect.orderItem.domain.OrderItemStatus;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,29 +29,47 @@ public class OrderService {
     // 주문서들 중 이름이 같은 메뉴를 카운트 
     // 새 주문서 작성
     // 새 주문맵에 finalPrice와 함께 추가 
-    public Map<Integer, List<OrderItem>> createOrderMap(OrderRequest orderRequest) {
-        Map<Integer, List<OrderItem>> orderByTableNo = orderRepository.getOrderByTableNo(orderRequest.getTableNo());
-        Map<Integer, List<OrderItem>> finalOrderMap = new ConcurrentHashMap<>();
+    public Map<Integer, Order> createOrderMap(OrderRequest orderRequest) {
+        int tableNo = orderRequest.getTableNo();
+        Map<Integer, List<OrderItem>> orderByTableNo = orderRepository.getOrderByTableNo(tableNo);
+
+        Map<Integer, Order> newOrderMapWithFinalPrice = new ConcurrentHashMap<>();
+        List<OrderItem> finalOrderItems = new ArrayList<>();
 
         for (Map.Entry<Integer, List<OrderItem>> entry : orderByTableNo.entrySet()) {
             List<OrderItem> orders = entry.getValue();
             for (OrderItem order : orders) {
                 int countByDuplicatedMenuName = countByDuplicatedMenuName(orders, order.getMenuName());
-                order.updateCount(countByDuplicatedMenuName);
-                orders.add(order);
-                finalOrderMap.put(order.getTableNo(), orders);
+                //order.updateCount(countByDuplicatedMenuName);
+                OrderItem newOrder = OrderItem.builder()
+                                                .tableNo(tableNo)
+                                                .menuName(order.getMenuName())
+                                                .price(order.getPrice())
+                                                .count(countByDuplicatedMenuName)
+                                                .totalPrice(order.addTotalPrice())
+                                                .isDiscount(order.isDiscount())
+                                                .itemstatus(OrderItemStatus.COMPLETED)
+                                                .build();
+                finalOrderItems.add(newOrder);
             }
         }
-        return finalOrderMap;
+
+        BigDecimal finalPrice = calculateFinalPrice(finalOrderItems);
+        Order finalOrder = Order.builder()
+                                .tableNo(tableNo)
+                                .orderList(finalOrderItems)
+                                .finalPrice(finalPrice)
+                                .status(OrderStatus.NOTSERVED)
+                                .build();
+        newOrderMapWithFinalPrice.put(tableNo, finalOrder);
+        return newOrderMapWithFinalPrice;
     }
 
-    /**
-     * 메뉴 갯수 취합
-     * 
-     * @param orders
-     * @param menuName
-     * @return
-     */
+    public Map<Integer, Order> allOrders(){
+        Map<Integer, List<OrderItem>> allOrdersFromRepo = orderRepository.getAllOrders();
+        return convertTypeByRepoMap(allOrdersFromRepo);
+    }
+
     public int countByDuplicatedMenuName(List<OrderItem> orders, String menuName) {
         int duplicatedCount = 0;
         for (OrderItem order : orders) {
@@ -57,4 +80,33 @@ public class OrderService {
         return duplicatedCount;
     }
 
+    public BigDecimal calculateFinalPrice(List<OrderItem> orders) {
+        BigDecimal finalPrice = BigDecimal.ZERO;
+        for (OrderItem order : orders) {
+                finalPrice = finalPrice.add(order.getTotalPrice());
+        }
+        return finalPrice;
+    }
+
+    private Map<Integer, Order> convertTypeByRepoMap(Map<Integer, List<OrderItem>> orderItemListMap) {
+        Map<Integer, Order> finalMap = new HashMap<>();
+        for(Map.Entry<Integer, List<OrderItem>> entry : orderItemListMap.entrySet()){
+            Integer tableNo = entry.getKey();
+            List<OrderItem> orders = entry.getValue();
+
+            BigDecimal finalPrice = calculateFinalPrice(orders);
+            Order finalOrder = Order.builder()
+                                    .tableNo(tableNo)
+                                    .orderList(orders)
+                                    .finalPrice(finalPrice)
+                                    .build();
+            finalMap.put(tableNo, finalOrder);
+        }
+        return finalMap;
+    }
+
+    public void clear(){
+        orderRepository.clear();
+    }
 }
+
