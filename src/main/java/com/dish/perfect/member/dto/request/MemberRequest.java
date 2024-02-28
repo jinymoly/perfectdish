0ace4695791a63639eb438489abb3420e4376603
jiny <zumod3naranja@gmail.com>
2024-02-28 15:09:31 +0900

    Fix : MemberRequest validation 수정

--- src/main/java/com/dish/perfect/member/dto/request/MemberRequest.java
+++ src/main/java/com/dish/perfect/member/dto/request/MemberRequest.java
@@ -3,7 +3,7 @@ package com.dish.perfect.member.dto.request;
 import com.dish.perfect.member.domain.MemberStatus;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class MemberRequest {
 
     @NotBlank(message = "휴대폰 번호는 필수입니다.")
-    @Size(min = 8, max = 8, message = "010을 제외한 나머지 8자리만 입력할 수 있습니다.")
+    @Pattern(regexp = "^[0-9]{8}$", message = "010을 제외한 나머지 8자리만 입력할 수 있습니다.")
     private String phoneNumber;
 
     @NotBlank(message = "이름은 필수입니다.")
--- src/main/java/com/dish/perfect/member/dto/request/MemberUpdateRequest.java
+++ src/main/java/com/dish/perfect/member/dto/request/MemberUpdateRequest.java
@@ -3,7 +3,7 @@ package com.dish.perfect.member.dto.request;
 import com.dish.perfect.member.domain.MemberStatus;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,11 +12,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class MemberUpdateRequest {
 
-
-    private Long id;
-
     @NotBlank(message = "휴대폰 번호는 필수입니다.")
-    @Size(min = 8, max = 8, message = "010을 제외한 나머지 8자리만 입력할 수 있습니다.")
+    @Pattern(regexp = "^[0-9]{8}$", message = "010을 제외한 나머지 8자리만 입력할 수 있습니다.")
     private String phoneNumber;
 
     @NotBlank(message = "이름은 필수입니다.")
@@ -24,9 +21,9 @@ public class MemberUpdateRequest {
 
     private MemberStatus status;
 
+
     @Builder
-    private MemberUpdateRequest(Long id, String phoneNumber, String userName, MemberStatus status) {
-        this.id = id;
+    private MemberUpdateRequest(String phoneNumber, String userName, MemberStatus status) {
         this.phoneNumber = phoneNumber;
         this.userName = userName;
         this.status = status;
