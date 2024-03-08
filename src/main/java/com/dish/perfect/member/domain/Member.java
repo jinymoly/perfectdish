3910a19325970ca903782e21f778160263b0bbe4
jiny <zumod3naranja@gmail.com>
2024-03-08 16:28:55 +0900

    Feat : Member 연관 관계 메서드 초기 작업

--- src/main/java/com/dish/perfect/member/domain/Member.java
+++ src/main/java/com/dish/perfect/member/domain/Member.java
@@ -36,7 +36,7 @@ public class Member {
 
     @CreatedDate
     @Column(updatable = false)
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
     private LocalDateTime modifiedAt;
@@ -46,27 +46,61 @@ public class Member {
     private MemberStatus status;
 
     @Builder
-    private Member(Long id, String userName, String phoneNumber, LocalDateTime createAt, LocalDateTime modifiedAt,
+    private Member(Long id, String userName, String phoneNumber, LocalDateTime createdAt, LocalDateTime modifiedAt,
             MemberStatus status) {
         this.id = id;
         this.userName = userName;
         this.phoneNumber = phoneNumber;
         this.status = status;
-        this.createAt = createAt;
+        this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
     }
 
-    @Override
-    public String toString() {
-        return "id='" + id + '\'' +
-                "  userName='" + userName + '\'' +
-                ", phoneNumber='" + phoneNumber + '\'' +
-                ", status='" + status + '\'' +
-                ", createAt=" + timeFomatter(createAt);
+    // @Override
+    // public String toString() {
+    //     return "id='" + id + '\'' +
+    //             "  userName='" + userName + '\'' +
+    //             ", phoneNumber='" + phoneNumber + '\'' +
+    //             ", status='" + status + '\'' +
+    //             ", createdAt=" + timeFomatter(createdAt) + '\'' +
+    //             ", updatedAt=" + timeFomatter(modifiedAt);
+    // }
+
+    public static Member of(String userName, String phoneNumber, MemberStatus status) {
+        return Member.builder()
+                    .userName(userName)
+                    .phoneNumber(phoneNumber)
+                    .status(status)
+                    .build();
     }
 
     private String timeFomatter(LocalDateTime time) {
         return time.format(DateTimeFormatter.ofPattern("yyyy-MM-dd/hh:MM:ss"));
     }
 
+    public void updateMemberInfo(String userName, String phoneNumber, LocalDateTime modifiedAt) {
+        this.userName = userName;
+        this.phoneNumber = phoneNumber;
+        this.modifiedAt = modifiedAt;
+    }
+
+    public boolean isNewUserName(final String inputName) {
+        return !(userName.equals(inputName));
+    }
+
+    public boolean isNewPhoneNumber(final String inputNumber){
+        return !(phoneNumber.equals(inputNumber));
+    }
+
+    public void updateMemberStatus(MemberStatus status, LocalDateTime modifiedAt) {
+        this.status = status;
+        this.modifiedAt = modifiedAt;
+    }
+
+    public boolean isActive(){
+        if(this.getStatus().equals(MemberStatus.ACTIVE)){
+            return true;
+        }
+        return false;
+    }
 }
