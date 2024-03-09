eeb66d6b3bc95158924a68113d5bdc202033561c
jiny <zumod3naranja@gmail.com>
2024-03-10 00:12:07 +0900

    Test : MemberRepositoryTest 객체 등록 및 db 확인

--- src/test/java/com/dish/perfect/member/domain/repository/MemberRepositoryTest.java
+++ src/test/java/com/dish/perfect/member/domain/repository/MemberRepositoryTest.java
@@ -1,10 +1,6 @@
 package com.dish.perfect.member.domain.repository;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotSame;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.List;
 
@@ -17,89 +13,47 @@ import org.springframework.boot.test.context.SpringBootTest;
 import com.dish.perfect.member.MemberFixture;
 import com.dish.perfect.member.domain.Member;
 import com.dish.perfect.member.domain.MemberStatus;
-import com.dish.perfect.member.dto.request.MemberUpdateRequest;
-
-import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
-@Slf4j
 public class MemberRepositoryTest {
 
     @Autowired
-    private InMemoryMemberRepository memberRepository;
+    private MemberRepository memberRepository;
 
     private MemberFixture fixtureM = new MemberFixture();
 
     @AfterEach
-    void clear(){
-        memberRepository.clear();
+    void repositoryClear() {
+        memberRepository.deleteAllInBatch();
     }
+
     @Test
     @DisplayName("유저 등록시 status가 ACTIVE")
     void save() {
-        Member member = memberRepository.save(fixtureM.fixtureA());
-        assertEquals(MemberStatus.ACTIVE, member.getStatus());
+        Member member = memberRepository.save(fixtureM.fixtureMB());
+        assertEquals(member.getStatus(), MemberStatus.ACTIVE);
     }
 
     @Test
-    @DisplayName("저장 - ConcurrentHashMap 동작")
+    @DisplayName("Member 저장")
     void savesametime() {
-        memberRepository.save(fixtureM.fixtureC());
-        memberRepository.save(fixtureM.fixtureB());
-        memberRepository.save(fixtureM.fixtureD());
-        memberRepository.save(fixtureM.fixtureE());
-        memberRepository.save(fixtureM.fixtureF());
+        memberRepository.save(fixtureM.fixtureMC());
+        memberRepository.save(fixtureM.fixtureMD());
         List<Member> findAll = memberRepository.findAll();
-        log.info("{}", findAll);
+
+        assertEquals(findAll.size(), 2);
 
     }
 
     @Test
-    @DisplayName("회원 조회 흐름 - filter : 이름 -> filter : 번호")
+    @DisplayName("같은 이름의 회원 등록 가능")
     void findMemberByNameFirst() {
-        Member memberA = memberRepository.save(fixtureM.fixtureA());
-        Member memberB = memberRepository.save(fixtureM.fixtureAD());
-        List<Member> findByNameList = memberRepository.findByName(memberB.getUserName());
+        Member memberA = memberRepository.save(fixtureM.fixtureMA());
+        Member memberB = memberRepository.save(fixtureM.fixtureMA_D());
 
-        Member result = memberRepository.findByPhoneNumberWithList(findByNameList, memberB.getPhoneNumber());
-        
-        assertSame(memberA.getUserName(), memberB.getUserName());
-        assertNotSame(memberA.getPhoneNumber(), memberB.getPhoneNumber());
-        assertEquals(memberB.getPhoneNumber(), result.getPhoneNumber());
+        assertEquals(memberA.getUserName(), memberB.getUserName());
     }
 
-    @Test
-    @DisplayName("회원 삭제시 status가 DELETED로 변경")
-    void deleteMember() {
-        Member newMemA = memberRepository.save(fixtureM.fixtureA());
 
-        List<Member> membersBysameName = memberRepository.findByName(newMemA.getUserName());
-        Member findMember = memberRepository.findByPhoneNumberWithList(membersBysameName, "22223333");
-        
-        memberRepository.deleteMember(findMember);
-
-        List<Member> findByName = memberRepository.findByName(findMember.getUserName());
-        Member result = memberRepository.findByPhoneNumberWithList(findByName, findMember.getPhoneNumber());
-
-        assertEquals(result.getStatus(), MemberStatus.DELETED);
-
-        
-    }
-    
-    @Test
-    @DisplayName("회원 정보 변경")
-    void updateMemberInfo(){
-        Member newMemF = memberRepository.save(fixtureM.fixtureF());
-        List<Member> findByName = memberRepository.findByName(newMemF.getUserName());
-        Member member = memberRepository.findByPhoneNumberWithList(findByName, newMemF.getPhoneNumber());
-        MemberUpdateRequest memberUpdateRequest = MemberUpdateRequest.builder()
-                                                        .userName("update")
-                                                        .phoneNumber("99999999")
-                                                        .status(member.getStatus())
-                                                        .build();
-        memberRepository.update(newMemF.getId(), memberUpdateRequest);
-        
-        assertNotEquals(newMemF.getUserName(), "update");
-    }
 
 }
