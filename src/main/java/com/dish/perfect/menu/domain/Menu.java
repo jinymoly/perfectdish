befe734a558dbcc8391999fe1d8bf563d68ea53e
jiny <zumod3naranja@gmail.com>
2024-03-14 23:49:04 +0900

    Refactor : Menu entity init

--- src/main/java/com/dish/perfect/menu/domain/Menu.java
+++ src/main/java/com/dish/perfect/menu/domain/Menu.java
@@ -1,30 +1,53 @@
 package com.dish.perfect.menu.domain;
 
+import org.springframework.data.annotation.Id;
+
 import com.dish.perfect.imageManager.domain.ImageFile;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EqualsAndHashCode(of = "menuName")
 public class Menu {
 
-    private CourseType courseType;
+    @Id
+    @Column(name = "menu_name")
     private String menuName;
+
+    @Column(nullable = false)
     private Integer price;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private CourseType courseType;
+
     private String description;
 
+    @OneToOne(fetch = FetchType.LAZY)
     private ImageFile menuImg;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private Availability availability;
 
+    @Column(nullable = false)
     private boolean isDiscounted;
 
     @Builder
-    public Menu(CourseType courseType, String menuName, Integer price, boolean isDiscounted, String description, ImageFile menuImg, Availability availability) {
+    public Menu(CourseType courseType, String menuName, Integer price, boolean isDiscounted, String description,
+            ImageFile menuImg, Availability availability) {
         this.courseType = courseType;
         this.menuName = menuName;
         this.price = price;
@@ -37,14 +60,18 @@ public class Menu {
     @Override
     public String toString() {
         return "[" + typeConverter(courseType) + "] "
-                + menuName + " || " + description + " (" + price + ") "+ availability +", D: "+ isDiscounted;
+                + menuName + " || " + description + " (" + price + ") " + availability + ", D: " + isDiscounted;
     }
 
-    private String typeConverter(CourseType type){
+    private String typeConverter(CourseType type) {
         return type.toString().replace("T_", "");
     }
 
-    public void addDiscount(boolean discount){
+    public void addDiscount(boolean discount) {
         this.isDiscounted = true;
+    }
+    
+    public void addImgFile(ImageFile file){
+        this.menuImg = file;
     }
 }
