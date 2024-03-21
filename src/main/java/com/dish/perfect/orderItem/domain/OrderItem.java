af8311da9d140c742ca8c14fd2e6c4cb8cf84014
jiny <zumod3naranja@gmail.com>
2024-03-21 23:55:21 +0900

    Refactor: Rebuild OrderItem 엔티티 구조

--- src/main/java/com/dish/perfect/orderItem/domain/OrderItem.java
+++ src/main/java/com/dish/perfect/orderItem/domain/OrderItem.java
@@ -2,49 +2,79 @@ package com.dish.perfect.orderItem.domain;
 
 import java.math.BigDecimal;
 
+import com.dish.perfect.menu.domain.Menu;
+import com.dish.perfect.order.domain.Order;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OrderBy;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Entity
 public class OrderItem {
 
-    private int tableNo;
+    @Id @GeneratedValue
+    @Column(name = "orderitem_id")
+    private Long id;
 
-    private String menuName;
+    @OneToMany
+    private Menu menu;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "table_no")
+    private Order order;
+
     private int count;
     private Integer price;
     
-    private boolean isDiscounted;
     private BigDecimal totalPrice;
     
     private OrderItemStatus itemstatus;
 
     @Builder
-    public OrderItem(int tableNo, String menuName, int count, Integer price,
-            BigDecimal totalPrice, OrderItemStatus itemstatus, boolean isDiscounted) {
-        this.tableNo = tableNo;
-        this.menuName = menuName;
+    public OrderItem(Long id,
+                    Order order,
+                    Menu menu, 
+                    int count,
+                    BigDecimal totalPrice, 
+                    OrderItemStatus itemstatus) {
+        this.id = id;
+        this.order = applyOrderTableNo(order.getTableNo());
+        this.menu = menu;
         this.count = count;
-        this.price = price;
-        this.totalPrice = addTotalPrice();
+        this.price = menu.getPrice();
+        this.totalPrice = applyTotalPrice();
         this.itemstatus = itemstatus;
-        this.isDiscounted = isDiscounted;
     }
 
     @Override
     public String toString() {
-        return menuName + ", " + price + "원, " + count + "개, total: " + totalPrice
-                + "원, [" + itemstatus + "], D: " + isDiscounted;
+        return menu.getMenuName() + ", " + price + "원, " + count + "개, total: " + totalPrice
+                + "원, [" + itemstatus + "], D: " + menu.isDiscounted();
     }
 
-    public BigDecimal addTotalPrice(){
-        if(isDiscounted){
+    public BigDecimal applyTotalPrice(){
+        if(menu.isDiscounted()){
            price = applyDiscount(price);
         }
         int total = price * count;
         return new BigDecimal(total);
+    }
+
+    public Order applyOrderTableNo(String tableNo){
+        return Order.builder()
+                    .tableNo(tableNo)
+                    .build();
     }
 
     public int applyDiscount(Integer price){
