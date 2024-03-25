ead2c35e51cfe9eb7fc6044b3c39f916471ad02d
jiny <zumod3naranja@gmail.com>
2024-03-25 23:32:29 +0900

    Fix : OrderItemRequest 수정

--- src/main/java/com/dish/perfect/orderItem/dto/request/OrderItemRequest.java
+++ src/main/java/com/dish/perfect/orderItem/dto/request/OrderItemRequest.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.dish.perfect.menu.domain.Menu;
+import com.dish.perfect.order.domain.Order;
 import com.dish.perfect.orderItem.domain.OrderItem;
 import com.dish.perfect.orderItem.domain.OrderItemStatus;
 
@@ -13,24 +14,33 @@ import lombok.Getter;
 @Getter
 public class OrderItemRequest {
 
-    private final Menu menu;
+    //private final Menu menu;
+
+    // TO-DO
+    // 메뉴 이름으로 메뉴 객체를 생성 하여 OrderItem에 Menu를 초기화 할 때
+    // OrderItemCoreService에서 OrderItem save 시 메뉴에 해당 메뉴 이름을 가진 메뉴가 있는지 확인 후 
+    // 있다면 저장 아니라면 예외처리 할 것 
+    private final String menuName;
     private final int count;
+
+    private final String tableNo;
     
     private final OrderItemStatus status;
 
     @Builder
-    private OrderItemRequest(Menu menu, int count, OrderItemStatus status){
-        this.menu = menu;
+    private OrderItemRequest(String menuName, int count, String tableNo, OrderItemStatus status){
+        this.menuName = menuName;
         this.count = count;
+        this.tableNo = tableNo;
         this.status = status;
     }
 
     public OrderItem toEntity(){
-        Map<Menu, Integer> newOrderItem = new HashMap<>();
-        newOrderItem.put(new Menu(menu.getMenuName()), Integer.valueOf(count));
         return OrderItem.builder()
-                        .orderItem(newOrderItem)
-                        .itemstatus(OrderItemStatus.CREATED)
+                        .menu(new Menu(menuName))
+                        .count(count)
+                        .order(new Order(tableNo))
+                        .orderItemStatus(OrderItemStatus.CREATED)
                         .build();
     }
 }
