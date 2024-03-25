1c65285e42026c5a1373a426cf1dc760dc35cd98
jiny <zumod3naranja@gmail.com>
2024-03-25 23:32:04 +0900

    Fix : OrderItem entity 수정

--- src/main/java/com/dish/perfect/orderItem/domain/OrderItem.java
+++ src/main/java/com/dish/perfect/orderItem/domain/OrderItem.java
@@ -1,15 +1,11 @@
 package com.dish.perfect.orderItem.domain;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
 
 import com.dish.perfect.menu.domain.Menu;
 import com.dish.perfect.order.domain.Order;
 
-import jakarta.persistence.CollectionTable;
 import jakarta.persistence.Column;
-import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
@@ -19,7 +15,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapKeyColumn;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,15 +29,14 @@ public class OrderItem {
     @Column(name = "orderItem_id")
     private Long id;
 
-    @ElementCollection
-    @CollectionTable(name = "orderItem_map", joinColumns = @JoinColumn(name = "orderItem_id"))
-    @MapKeyColumn(name = "orderItem_menu")
-    @Column(name = "orderItem_count")
-    private Map<Menu, Integer> orderItemMap = new HashMap<>();
-
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "table_no")
+    @JoinColumn(name = "order_id")
     private Order order;
+
+    @ManyToOne
+    private Menu menu;
+
+    private int count;
     
     @Enumerated(EnumType.STRING)
     private OrderItemStatus orderItemStatus;
@@ -50,14 +44,19 @@ public class OrderItem {
     private LocalDateTime createdAt;
 
     @Builder
-    public OrderItem(Long id, Map<Menu, Integer> orderItem, Order order, OrderItemStatus orderItemStatus, LocalDateTime createdAt) {
+    public OrderItem(Long id, Menu menu, int count, Order order, OrderItemStatus orderItemStatus, LocalDateTime createdAt) {
         this.id = id;
-        this.orderItemMap = new HashMap<>(orderItem);
-        this.order = initOrderFrom(order.getTableNo());
+        this.order = order;
+        this.count = count;
+        this.menu = initMenuFrom(menu.getMenuName());
         this.orderItemStatus = orderItemStatus;
         this.createdAt = createdAt;
     }
-    
+
+    private Menu initMenuFrom(String menuName){
+        return new Menu(menuName);
+    }
+
     private Order initOrderFrom(String tableNo){
         return new Order(tableNo);
     }
