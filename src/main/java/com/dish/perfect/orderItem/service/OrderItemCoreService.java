72484195b02c3229b5d112586afbd5fcd7e829d1
jiny <zumod3naranja@gmail.com>
2024-03-26 23:28:09 +0900

    Fix : OrderItemCoreService create조건 추가

--- src/main/java/com/dish/perfect/orderItem/service/OrderItemCoreService.java
+++ src/main/java/com/dish/perfect/orderItem/service/OrderItemCoreService.java
@@ -1,17 +1,17 @@
 package com.dish.perfect.orderItem.service;
 
-import java.util.Optional;
+import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dish.perfect.global.error.GlobalException;
 import com.dish.perfect.global.error.exception.ErrorCode;
+import com.dish.perfect.menu.service.MenuPresentationService;
 import com.dish.perfect.orderItem.domain.OrderItem;
 import com.dish.perfect.orderItem.domain.repository.OrderItemRepository;
 import com.dish.perfect.orderItem.dto.request.OrderItemRequest;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -20,28 +20,27 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Slf4j
 public class OrderItemCoreService {
-    
-    private final OrderItemRepository orderItemRepository;
 
-    public OrderItem createOrderItem(OrderItemRequest orderItemRequest){
+    private final OrderItemRepository orderItemRepository;
+    private final MenuPresentationService menuService;
+
+    public OrderItem createOrderItem(OrderItemRequest orderItemRequest) {
         OrderItem orderItem = orderItemRequest.toEntity();
-        OrderItem newOrder = orderItemRepository.save(orderItem);
-        log.info("createOrderItem{}\nid:{}\nmenu:{}\ncount:{}\nD:{}/price:{}", 
-                        newOrder.getId(), 
-                        newOrder.getMenu().getMenuName(),
-                        newOrder.getCount(),
-                        newOrder.getMenu().isDiscounted(),
-                        newOrder.getTotalPrice());
-        return newOrder;
+        orderItem.addCreatedAt(LocalDateTime.now());
+        if(menuService.menuNameExists(orderItem.getMenu().getMenuName())){
+            orderItemRepository.save(orderItem);
+            log.info("{}/주문 완료", orderItem.getMenu().getMenuName());
+            return orderItem;
+        }else{
+            throw new GlobalException(ErrorCode.NOT_FOUND_MENU, "메뉴를 담을 수 없습니다.");
+        }
     }
 
-    public OrderItem updateOrderItemCount(final OrderItem orderItem, @Valid OrderItemRequest orderItemRequest){
-        if(orderItem.getMenu().equals(orderItemRequest.getMenu())){
-            orderItem.updateCount(orderItemRequest.getCount());
-            return orderItemRepository.save(orderItem);
-        } else {
-            throw new GlobalException(ErrorCode.NOT_FOUND_MENU, "해당 메뉴가 존재하지 않습니다.");
-        }
-
+    public void updateOrderItemStatus(Long id){
+        OrderItem orderItem = orderItemRepository.findById(id)
+                                                .orElseThrow(() -> new GlobalException(ErrorCode.NOT_FOUND_ORDERITEM, "주문 아이템이 존재하지 않습니다."));
+        orderItem.markOrderItemStatusAsCompleted();
+        orderItemRepository.save(orderItem);
+        log.info("{}/{}", orderItem.getId(), orderItem.getOrderItemStatus());
     }
 }
