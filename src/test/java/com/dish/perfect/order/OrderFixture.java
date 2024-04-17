c32352558cafeda8e1d2fb998f5dc5cb1729ae59
jiny <zumod3naranja@gmail.com>
2024-04-17 09:42:02 +0900

    Test : fix OrderItem -> Order

--- src/test/java/com/dish/perfect/order/OrderFixture.java
+++ src/test/java/com/dish/perfect/order/OrderFixture.java
@@ -1,81 +1,94 @@
 package com.dish.perfect.order;
 
-import java.util.ArrayList;
-import java.util.List;
 
+import com.dish.perfect.menu.MenuFixture;
+import com.dish.perfect.menu.domain.Menu;
 import com.dish.perfect.order.domain.OrderStatus;
 import com.dish.perfect.order.dto.request.OrderRequest;
-import com.dish.perfect.orderItem.OrderItemFixture;
-import com.dish.perfect.orderItem.domain.OrderItem;
-
 
 public class OrderFixture {
-    
-    private OrderItemFixture orderItemFixture = new OrderItemFixture();
-    
-    private OrderItem orderItemA = orderItemFixture.orderItemRequestA.toEntity();
-    private OrderItem orderItemB = orderItemFixture.orderItemRequestB.toEntity();
-    private OrderItem orderItemC = orderItemFixture.orderItemRequestC.toEntity();
-    private OrderItem orderItemD = orderItemFixture.orderItemRequestD.toEntity();
-    private OrderItem orderItemE = orderItemFixture.orderItemRequestE.toEntity();
-    private OrderItem orderItemF = orderItemFixture.orderItemRequestF.toEntity();
-    private OrderItem orderItemG = orderItemFixture.orderItemRequestG.toEntity();
-    private OrderItem orderItemH = orderItemFixture.orderItemRequestH.toEntity();
-    private OrderItem orderItemI = orderItemFixture.orderItemRequestI.toEntity();
-    private OrderItem orderItemJ = orderItemFixture.orderItemRequestJ.toEntity();
-    
-    public OrderRequest orderRequest1 = OrderRequest.builder()
-                                                    .tableNo("2")
-                                                    .orderItems(orderItemsT2())
-                                                    .status(OrderStatus.NOTSERVED)
-                                                    .build();
 
-    public OrderRequest orderRequest2 = OrderRequest.builder()
-                                                    .tableNo("3")
-                                                    .orderItems(orderItemsT3())
-                                                    .status(OrderStatus.NOTSERVED)
-                                                    .build();
-    public OrderRequest orderRequest3 = OrderRequest.builder()
-                                                    .tableNo("7")
-                                                    .orderItems(orderItemsT7())
-                                                    .status(OrderStatus.NOTSERVED)
-                                                    .build();
-    public OrderRequest orderRequest3D = OrderRequest.builder()
-                                                    .tableNo("1")
-                                                    .orderItems(orderItemsT7andT1())
-                                                    .status(OrderStatus.NOTSERVED)
-                                                    .build();
+    private MenuFixture menuFixture = new MenuFixture();
 
-    
-    public List<OrderItem> orderItemsT2(){
-        List<OrderItem> orderItems = new ArrayList<>();
-        orderItems.add(orderItemA);
-        orderItems.add(orderItemE);
-        return orderItems;
-    }
+    private Menu menuA = menuFixture.fixRequestA().toEntity();
+    private Menu menuB = menuFixture.fixRequestB().toEntity();
+    private Menu menuC = menuFixture.fixRequestC().toEntity();
+    private Menu menuD = menuFixture.fixRequestD().toEntity();
+    private Menu menuE = menuFixture.fixRequestE().toEntity();
+    private Menu menuF = menuFixture.fixRequestF().toEntity();
+    private Menu menuG = menuFixture.fixRequestG().toEntity();
+    private Menu menuH = menuFixture.fixRequestH().toEntity();
 
-    public List<OrderItem> orderItemsT3(){
-        List<OrderItem> orderItems = new ArrayList<>();
-        orderItems.add(orderItemB);
-        orderItems.add(orderItemC);
-        orderItems.add(orderItemD);
-        return orderItems;
-    }
+    public OrderRequest orderRequestA = OrderRequest.builder()
+                                                                .tableNo("2")
+                                                                .menuName(menuA.getMenuName())
+                                                                .count(2)
+                                                                .status(OrderStatus.CREATED)
+                                                                .build();
 
-    public List<OrderItem> orderItemsT7(){
-        List<OrderItem> orderItems = new ArrayList<>();
-        orderItems.add(orderItemF);
-        orderItems.add(orderItemG);
-        return orderItems;
-    }
+    public OrderRequest orderRequestB = OrderRequest.builder()
+                                                                .tableNo("3")
+                                                                .menuName(menuB.getMenuName())
+                                                                .count(1)
+                                                                .status(OrderStatus.CREATED)
+                                                                .build();
 
-    public List<OrderItem> orderItemsT7andT1(){
-        List<OrderItem> orderItems = new ArrayList<>();
-        orderItems.add(orderItemH);
-        orderItems.add(orderItemI);
-        orderItems.add(orderItemJ);
-        return orderItems;
-    }
+    public OrderRequest orderRequestC = OrderRequest.builder()
+                                                                .tableNo("3")
+                                                                .menuName(menuC.getMenuName())
+                                                                .count(3)
+                                                                .status(OrderStatus.CREATED)
+                                                                .build();
 
-    
+    public OrderRequest orderRequestD = OrderRequest.builder()
+                                                                .tableNo("3")
+                                                                .menuName(menuD.getMenuName())
+                                                                .count(5)
+                                                                .status(OrderStatus.CREATED)
+                                                                .build();
+
+    public OrderRequest orderRequestE = OrderRequest.builder()
+                                                                .tableNo("2")
+                                                                .menuName(menuE.getMenuName())
+                                                                .count(2)
+                                                                .status(OrderStatus.CREATED)
+                                                                .build();
+
+    public OrderRequest orderRequestF = OrderRequest.builder()
+                                                                .tableNo("7")
+                                                                .menuName(menuF.getMenuName())
+                                                                .count(2)
+                                                                .status(OrderStatus.COMPLETED)
+                                                                .build();
+
+    public OrderRequest orderRequestG = OrderRequest.builder()
+                                                                .tableNo("7")
+                                                                .menuName(menuG.getMenuName())
+                                                                .count(3)
+                                                                .status(OrderStatus.CREATED)
+                                                                .build();
+
+    public OrderRequest orderRequestH = OrderRequest.builder()
+                                                                .tableNo("7")
+                                                                .menuName(menuH.getMenuName())
+                                                                .count(1)
+                                                                .status(OrderStatus.COMPLETED)
+                                                                .build();
+
+    public OrderRequest orderRequestI = OrderRequest.builder()
+                                                                .tableNo("1")
+                                                                .menuName(menuH.getMenuName())
+                                                                .count(1)
+                                                                .status(OrderStatus.COMPLETED)
+                                                                .build();
+
+    public OrderRequest orderRequestJ = OrderRequest.builder()
+                                                                .tableNo("1")
+                                                                .menuName(menuB.getMenuName())
+                                                                .count(3)
+                                                                .status(OrderStatus.CREATED)
+                                                                .build();
+
+
+
 }
--- src/test/java/com/dish/perfect/order/domain/repository/OrderRepositoryTest.java
+++ src/test/java/com/dish/perfect/order/domain/repository/OrderRepositoryTest.java
@@ -1,9 +1,6 @@
 package com.dish.perfect.order.domain.repository;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.List;
-import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,47 +9,49 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.dish.perfect.menu.MenuFixture;
+import com.dish.perfect.menu.domain.repository.MenuRepository;
 import com.dish.perfect.order.OrderFixture;
-import com.dish.perfect.orderItem.domain.OrderItem;
-
-import lombok.extern.slf4j.Slf4j;
+import com.dish.perfect.order.domain.Order;
+import com.dish.perfect.order.domain.OrderStatus;
 
 @SpringBootTest
 public class OrderRepositoryTest {
 
     @Autowired
-    private InMemoryOrderRepository orderRepository;
+    private OrderRepository orderRepository;
+    @Autowired
+    private MenuRepository menuRepository;
 
     private OrderFixture fixtureO = new OrderFixture();
-
-    @BeforeEach
-    void saveFixture() {
-        Map<Integer, List<OrderItem>> table1 = orderRepository.saveOrderMap(fixtureO.orderRequest1);
-        Map<Integer, List<OrderItem>> table2 = orderRepository.saveOrderMap(fixtureO.orderRequest2);
-        Map<Integer, List<OrderItem>> table3 = orderRepository.saveOrderMap(fixtureO.orderRequest3);
-    }
+    private MenuFixture fixtureM = new MenuFixture();
 
     @AfterEach
     void clear() {
-        orderRepository.clear();
+        orderRepository.deleteAllInBatch();
+    }
+
+    @BeforeEach
+    void createMenu() {
+        menuRepository.save(fixtureM.fixRequestA().toEntity());
+        menuRepository.save(fixtureM.fixRequestB().toEntity());
+        menuRepository.save(fixtureM.fixRequestC().toEntity());
+        menuRepository.save(fixtureM.fixRequestD().toEntity());
+        menuRepository.save(fixtureM.fixRequestE().toEntity());
+        menuRepository.save(fixtureM.fixRequestF().toEntity());
+        menuRepository.save(fixtureM.fixRequestG().toEntity());
+        menuRepository.save(fixtureM.fixRequestH().toEntity());
+        menuRepository.save(fixtureM.fixRequestI().toEntity());
     }
 
     @Test
-    @DisplayName("최종 주문서 저장 및 모든 주문 및 조회")
-    void createFinalOrderAndAllOrders() {
+    @DisplayName("주문 생성시 status.CREATED")
+    void createOrder() {
+        Order orderB = orderRepository.save(fixtureO.orderRequestB.toEntity());
+        assertTrue(orderB.getOrderStatus().equals(OrderStatus.CREATED));
 
-        Map<Integer, List<OrderItem>> orders = orderRepository.getAllOrders();
-        assertThat(orders.containsKey(1)).isTrue();
-        assertThat(orders.containsKey(2)).isTrue();
-        assertThat(orders.containsKey(3)).isTrue();
     }
 
-    @Test
-    @DisplayName("테이블 번호 별 주문 조회")
-    void getOrdersByTableNo() {
-        Map<Integer, List<OrderItem>> table2 = orderRepository.getOrderByTableNo(2);
-        assertThat(table2.containsKey(2)).isTrue();
-        
-    }
+    
 
 }
