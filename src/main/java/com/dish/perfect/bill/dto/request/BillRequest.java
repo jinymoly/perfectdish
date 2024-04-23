c7970379d0f2933579beb32e6bd8802da7ffb62d
jiny <zumod3naranja@gmail.com>
2024-04-23 23:49:01 +0900

    Fix : BillRequest -> Bill 객체 생성 시 orders 서로 다른 객체로

--- src/main/java/com/dish/perfect/bill/dto/request/BillRequest.java
+++ src/main/java/com/dish/perfect/bill/dto/request/BillRequest.java
@@ -1,5 +1,6 @@
 package com.dish.perfect.bill.dto.request;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.dish.perfect.bill.domain.Bill;
@@ -11,10 +12,6 @@ import lombok.Getter;
 
 @Getter
 public class BillRequest {
-
-    // TO-DO
-    // member 따로 Point Entity로 관리할건지
-    // Order에서 Member정보가 무조건 필요하지 않음 >> 회원/비회원
 
     private final String tableNo;
 
@@ -29,9 +26,11 @@ public class BillRequest {
     }
 
     public Bill toBill() {
+        List<Order> ordersFromRequest = new ArrayList<>(orders); 
+
         return Bill.builder()
                 .tableNo(tableNo)
-                .orders(orders)
+                .orders(ordersFromRequest)
                 .orderStatus(status)
                 .build();
     }
