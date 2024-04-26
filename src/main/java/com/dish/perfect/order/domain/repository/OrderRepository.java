ba4fc00d21ad41e656b31597f2de5378606c9d9e
jiny <zumod3naranja@gmail.com>
2024-04-26 23:04:31 +0900

    Feat : OrderRepository 쿼리 추가

--- src/main/java/com/dish/perfect/order/domain/repository/OrderRepository.java
+++ src/main/java/com/dish/perfect/order/domain/repository/OrderRepository.java
@@ -14,6 +14,26 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findBytableNo(String tableNo);
 
-    @Query("select oi from Order oi where oi.orderStatus = :orderStatus")
+    @Query("select o from Order o where o.orderStatus = :orderStatus")
     List<Order> findByOrderStatus(@Param("orderStatus")OrderStatus status);
+
+    /**
+      * 테이블 번호가 같고 메뉴이름이 같은 주문 카운트 
+      * db에서 집계 먼저
+      * @param tableNo
+      * @return
+      */
+    @Query("SELECT COUNT(o.menu) FROM Order o WHERE o.tableNo = :tableNo GROUP BY oi.menu.menuName")
+    int countMenuNameWithSameTableNo(@Param("tableNo") String tableNo);
+
+    /**
+     * 테이블 번호가 같고 메뉴이름이 같은 주문 카운트 
+     * 리스트로 반환하여 service에서 카운트
+     * @param tableNo
+     * @param menuName
+     * @return
+     */
+    @Query("SELECT o FROM Order o JOIN o.menu m WHERE o.tableNo = :tableNo AND o.orderStatus = :status AND m.menuName = :menuName")
+    List<Order> findByCompletedOrderAndSameMenuNameWithSameTableNo(@Param("tableNo") String tableNo, @Param("status") OrderStatus status, @Param("menuName") String menuName);
+
 }
