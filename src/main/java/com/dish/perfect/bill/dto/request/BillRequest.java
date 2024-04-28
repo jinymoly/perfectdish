f38bfabf3e2d00ee64169580fbb24effcc0152cc
jiny <zumod3naranja@gmail.com>
2024-04-28 17:48:07 +0900

    Fix : BillRequest toBill 이동 및 필드 수정

--- src/main/java/com/dish/perfect/bill/dto/request/BillRequest.java
+++ src/main/java/com/dish/perfect/bill/dto/request/BillRequest.java
@@ -1,11 +1,6 @@
 package com.dish.perfect.bill.dto.request;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.dish.perfect.bill.domain.Bill;
 import com.dish.perfect.bill.domain.BillStatus;
-import com.dish.perfect.order.domain.Order;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -14,24 +9,11 @@ import lombok.Getter;
 public class BillRequest {
 
     private final String tableNo;
-
-    private final List<Order> orders;
     private final BillStatus status;
 
     @Builder
-    private BillRequest(String tableNo, List<Order> orders, BillStatus status) {
+    private BillRequest(String tableNo, BillStatus status) {
         this.tableNo = tableNo;
-        this.orders = orders;
-        this.status = BillStatus.NOTSERVED;
-    }
-
-    public Bill toBill() {
-        List<Order> ordersFromRequest = new ArrayList<>(orders); 
-
-        return Bill.builder()
-                .tableNo(tableNo)
-                .orders(ordersFromRequest)
-                .orderStatus(status)
-                .build();
+        this.status = status;
     }
 }
