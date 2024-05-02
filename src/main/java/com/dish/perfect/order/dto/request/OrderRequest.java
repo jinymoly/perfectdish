7adbc2233a20889b7389c4fc2d9044c3ea6587eb
jiny <zumod3naranja@gmail.com>
2024-05-02 22:44:53 +0900

    Fix : OrderRequest order객체 변환 이동

--- src/main/java/com/dish/perfect/order/dto/request/OrderRequest.java
+++ src/main/java/com/dish/perfect/order/dto/request/OrderRequest.java
@@ -1,7 +1,5 @@
 package com.dish.perfect.order.dto.request;
 
-import com.dish.perfect.menu.domain.Menu;
-import com.dish.perfect.order.domain.Order;
 import com.dish.perfect.order.domain.OrderStatus;
 
 import lombok.Builder;
@@ -17,27 +15,18 @@ public class OrderRequest {
     // OrderCoreService에서 Order save 시 메뉴에 해당 메뉴 이름을 가진 메뉴가 있는지 확인 후 
     // 있다면 저장 아니라면 예외처리 할 것 
     private final String menuName;
-    private final int count;
+    private final int quantity;
 
     private final String tableNo;
     
     private final OrderStatus status;
 
     @Builder
-    private OrderRequest(String menuName, int count, String tableNo, OrderStatus status){
+    private OrderRequest(String menuName, int quantity, String tableNo, OrderStatus status){
         this.menuName = menuName;
-        this.count = count;
+        this.quantity = quantity;
         this.tableNo = tableNo;
         this.status = status;
     }
 
-    public Order toOrder(){
-        return Order.builder()
-                        .tableNo(tableNo)   
-                        .menu(new Menu(menuName))
-                        .count(count)
-                        .tableNo(tableNo)
-                        .orderStatus(OrderStatus.CREATED)
-                        .build();
-    }
 }
