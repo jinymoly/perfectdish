23eacf23ff99d9e8987b54261f09097fbb1d1035
jiny <zumod3naranja@gmail.com>
2024-05-06 23:31:50 +0900

    Feat : BillController

---
+++ src/main/java/com/dish/perfect/bill/presentation/BillController.java
@@ -0,0 +1,50 @@
+package com.dish.perfect.bill.presentation;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dish.perfect.bill.domain.Bill;
+import com.dish.perfect.bill.dto.request.BillRequest;
+import com.dish.perfect.bill.dto.response.BillResponse;
+import com.dish.perfect.bill.service.BillCoreService;
+import com.dish.perfect.bill.service.BillPresentationService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("bill")
+public class BillController {
+    
+    private final BillCoreService billCoreService;
+    private final BillPresentationService billPresentationService;
+
+    @GetMapping("/create")
+    public String addBillRequest(){
+        return "BillRequest 페이지로 이동";
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<Bill> createOrder(@RequestBody @Valid BillRequest billRequest){
+        Bill newOrder = billCoreService.createBill(billRequest);
+        return ResponseEntity.ok(newOrder);
+    }
+
+    @GetMapping("/{tableNo}")
+    public ResponseEntity<BillResponse> getBillByTableNo(@PathVariable("tableNo") String tableNo) {
+        BillResponse bill = billPresentationService.findBillByTableNo(tableNo);
+        return ResponseEntity.ok(bill);
+    }
+    
+    
+}
