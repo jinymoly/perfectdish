767fd22d1cbad8d0761b632291f9c36d453228cb
jiny <zumod3naranja@gmail.com>
2026-03-05 01:59:50 +0900

    Feat : 회원 정보 수정 및 탈퇴 기능 구현

--- backend/src/main/java/com/dish/perfect/member/presentation/MemberController.java
+++ backend/src/main/java/com/dish/perfect/member/presentation/MemberController.java
@@ -15,13 +15,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.dish.perfect.global.auth.JwtTokenProvider;
-import com.dish.perfect.global.auth.service.AuthService;
 import com.dish.perfect.global.util.CookieUtil;
 import com.dish.perfect.member.domain.Member;
+import com.dish.perfect.member.domain.MemberStatus;
+import com.dish.perfect.member.dto.request.MemberChangeStatusRequest;
 import com.dish.perfect.member.dto.request.MemberLoginRequest;
 import com.dish.perfect.member.dto.request.MemberRequest;
+import com.dish.perfect.member.dto.request.MemberUpdateRequest;
+import com.dish.perfect.member.dto.response.MemberCommonResponse;
+import com.dish.perfect.member.dto.response.MemberDetailResponse;
 import com.dish.perfect.member.service.MemberCoreService;
+import com.dish.perfect.member.service.MemberPresentationService;
 
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -36,6 +40,8 @@ import lombok.extern.slf4j.Slf4j;
 public class MemberController {
 
     private final MemberCoreService memberCoreService;
+    private final MemberPresentationService memberPresentationService;
+    private final JwtTokenProvider jwtTokenProvider;
     private final JwtTokenProvider jwtTokenProvider;
     private final AuthService authService;
     private final CookieUtil cookieUtil;
@@ -78,6 +84,63 @@ public class MemberController {
         log.info("User logged in: {}", member.getPhoneNumber());
 
         return new ResponseEntity<>(loginInfo, HttpStatus.OK);
+    }
+
+    @GetMapping("/allmember/active")
+    public ResponseEntity<List<MemberCommonResponse>> getActiveMember() {
+        List<MemberCommonResponse> findAll = memberPresentationService.findAllWithActive();
+        return ResponseEntity.ok(findAll);
+    }
+
+    @GetMapping("/allmember")
+    public ResponseEntity<List<MemberCommonResponse>> getAllMember() {
+        List<MemberCommonResponse> findAll = memberPresentationService.findAll();
+        return ResponseEntity.ok(findAll);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberDetailResponse> getMemberInfo(@PathVariable("id") Long id) {
+        MemberDetailResponse member = memberPresentationService.getMemberInfo(id);
+        return ResponseEntity.ok(member);
+    }
+
+    @GetMapping("/findMemberInfoByNumber")
+    public ResponseEntity<MemberDetailResponse> findMemberByPhoneNumber(
+            @RequestParam("phoneNumber") String phoneNumber) {
+        MemberDetailResponse findByphoneNumber = memberPresentationService.findByPhoneNumber(phoneNumber);
+        return ResponseEntity.ok(findByphoneNumber);
+    }
+
+    @GetMapping("/{id}/edit")
+    public String updateMemberInfo(@PathVariable String id) {
+        return "MemberUpdateRequest 페이지로 이동";
+    }
+
+    @PatchMapping("/{id}/edit")
+    public ResponseEntity<Void> updateMemberInfo(@PathVariable Long id,
+            @RequestBody @Valid MemberUpdateRequest memberUrequest) {
+        Member member = memberPresentationService.findById(id);
+        memberCoreService.updateMemberInfo(member.getId(), memberUrequest);
+        log.info("update member:{}님/{}", memberUrequest.getUserName(), memberUrequest.getPhoneNumber());
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("{id}/delete")
+    public ResponseEntity<Void> deleteMember(@PathVariable("id") Long id,
+            @RequestBody @Valid MemberChangeStatusRequest memberDrequest) {
+        Member member = memberPresentationService.findById(id);
+        if (member == null) {
+            return ResponseEntity.noContent().build();
+        }
+        if (member.getStatus().equals(MemberStatus.DELETED)) {
+            log.warn("already deleted:{}[{}]", member.getUserName(), member.getStatus());
+            return ResponseEntity.noContent().build();
+        } else {
+            memberCoreService.deleteMemberByStatus(id, memberDrequest);
+            log.info("deleted member:{}님/{}", member.getUserName(), memberDrequest.getStatus());
+
+        }
+        return ResponseEntity.noContent().build();
     }
 }
 }
--- backend/src/main/java/com/dish/perfect/member/service/MemberCoreService.java
+++ backend/src/main/java/com/dish/perfect/member/service/MemberCoreService.java
@@ -9,8 +9,12 @@ import com.dish.perfect.global.error.exception.ErrorCode;
 import com.dish.perfect.member.domain.Member;
 import com.dish.perfect.member.domain.MemberStatus;
 import com.dish.perfect.member.domain.repository.MemberRepository;
+import com.dish.perfect.member.dto.request.MemberChangeStatusRequest;
 import com.dish.perfect.member.dto.request.MemberLoginRequest;
 import com.dish.perfect.member.dto.request.MemberRequest;
+import com.dish.perfect.member.dto.request.MemberUpdateRequest;
+
+import jakarta.validation.Valid;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -41,6 +45,36 @@ public class MemberCoreService {
             throw new GlobalException(ErrorCode.FAIL_LOGIN_MEMBER);
         }
         return member;
+    }
+
+    public Long updateMemberInfo(final Long memberId, @Valid MemberUpdateRequest updateRequest) {
+        Member member = findMemberById(memberId);
+        if (member.isNewUserName(updateRequest.getUserName())) {
+            validPhoneNumberDuplicated(updateRequest.getPhoneNumber());
+        }
+        member.updateMemberInfo(updateRequest.getUserName(),
+                updateRequest.getPhoneNumber());
+        return member.getId();
+    }
+
+    public Long deleteMemberByStatus(final Long memberId, MemberChangeStatusRequest statusRequest) {
+        Member member = findMemberById(memberId);
+        if (!member.getStatus().equals(MemberStatus.DELETED)) {
+            member.updateMemberStatus(statusRequest.getStatus());
+        }
+        new GlobalException(ErrorCode.ALREADY_DELETED_MEMBER, "이미 탈퇴한 사용자입니다.");
+        return member.getId();
+    }
+
+    private Member findMemberById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new GlobalException(ErrorCode.NOT_FOUND_MEMBER, memberId + "해당 회원이 존재하지 않습니다."));
+    }
+
+    private void validPhoneNumberDuplicated(String phoneNumber) {
+        if (memberRepository.existsByPhoneNumber(phoneNumber)) {
+            throw new GlobalException(ErrorCode.DUPLICATED_MEMBER_INFO, "해당 폰번호가 이미 존재합니다.");
+        }
     }
 
     private void validPhoneNumberDuplicatedByMember(Member member) {
